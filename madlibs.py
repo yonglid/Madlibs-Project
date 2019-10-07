23d6f51d4a37646bd45ae7f091a483c3f6d060cb
Yong Emily Dich <yongemilydich@Yongs-MacBook-Pro.local>
2019-10-07 03:10:20 -0400

    design doc

--- madlibs.py
+++ madlibs.py
@@ -1,17 +1,44 @@
 from textblob import TextBlob
 import random
 from collections import defaultdict
+import nltk
+nltk.download('punkt')
+# seems to be using a simple perceptron algorithm
+nltk.download('averaged_perceptron_tagger')
 class Poems:
   def __init__(self):
     self.madlibs_type_mapping = {1: "Computer", 2:"Generated", 3:"New"}
-    self.madlibs_tenses = {1: [(5, "noun"), (11, "adjective"), (15,"verb"), (18, "verb"), (26, "noun")]}
-    self.madlibs_library = {1:"I ate a ton of sugar. \n It made me very sweet. \n It also made me very round -- \n now I can't find my feet."}
+    self.madlibs_tenses = {1: [(5, "noun"), (10, "adjective"), (13,"verb"), (16, "verb"), (23, "noun")]}
+    self.madlibs_library = {1:"I ate a ton of sugar. It made me very sweet. It also made me very round -- now I can't find my feet."}
     self.madlibs_type_list = [f"{key}:{self.madlibs_type_mapping[key]}" for key in self.madlibs_type_mapping]
     self.madlibs_answers = defaultdict(list)
     self.multiplayer = False
     self.madlibs_type = 1
     self.player1 = ""
     self.player2 = ""
+    self.blob_map = {"noun": ["NN", "NNS", "NNP", "NNPS"], "verb": ["VBD", "VB", "VBG", "VBN", "VBP", "VBZ"], "coordinating conjunction": ["CC"], "cardinal digit": ["CD"], "determiner": ["DT"], "existential there": ["EX"], "foreign word": ["FW"], "preposition/subordinating conjunction": ["IN"],
+        "adjective": ["JJ", "JJR", "JJS"],
+        "adjective, comparative": ["JJR"],
+        "adjective, superlative": ["JJS"],
+        "list marker": ["LS"],
+        "modal": ["MD"],
+        "noun, singular": ["NN"],
+        "noun, plural":["NNS"],
+        "proper noun, singular": ["NNP"],
+        "proper noun, plural": ["NNPS"],
+        "predeterminer": ["PDT"],
+        "possessive ending": ["POS"],
+        "possessive pronoun": ["PRP$"],
+        "adverb": ["RB", "RBR", "RBS"],
+        "adverb, comparative": ["RBR"],
+        "adverb, superlative": ["RBS"],
+        "particle": ["RP"],
+        "to go": ["TO"],
+        "interjection": ["UH"],
+        "wh-determiner": ["WDT"],
+        "wh-pronoun": ["WP"],
+        "possessive wh-pronoun": ["WP$"],
+        "wh-adverb where": ["WRB"]}
 
   def preferences(self, multiplayer_yesno, lib_type_num):
       self.multiplayer = True if multiplayer_yesno == "Y" else False
@@ -41,6 +68,14 @@ class Poems:
       print("Original: " + str(origin))
       print("New: " + str(self.madlibs_answers[rand_lib]))
       print(' '.join(lib_to_list))
+      blob = TextBlob(' '.join(lib_to_list))
+      response = f""
+      for ind in range(0, len(self.madlibs_tenses[rand_lib])-1):
+          word_ind = self.madlibs_tenses[rand_lib][ind][0]
+          pos = self.madlibs_tenses[rand_lib][ind][1]
+          if blob.tags[word_ind][1] not in self.blob_map[pos]:
+              response += f"{blob.tags[word_ind]} is not {pos}. "
+      print(response)
       return ' '.join(lib_to_list)
 
 poem_inst = Poems()
